dd37789800b3a2ff366d8286105ae5b838eb096e
green-study <soo010410@naver.com>
2022-05-09 18:00:29 +0900

    homework upload

--- src/vo/TodoList.js
+++ src/vo/TodoList.js
@@ -8,11 +8,61 @@ class TodoList {
   _items = [];
   _date = "";
 
-  constructor() {
+  constructor(items, date) {
     makeObservable(this, {
       _items: observable,
     });
+    this._items = items;
+    this._date = date;
   }
+  get items() {
+    return this._items;
+  }
+
+  removeTodoItem = (todoId) =>{
+    const targetTodoItemIndex = this._items.findIndex(
+      (todo) => todo.id === todoId
+    );
+    //존재하지 않을 시 에러처리
+    if(targetTodoItemIndex === -1) return;
+    this._items.splice(targetTodoItemIndex,1);
+  }
+
+  pushTodoItem = (todoitem) =>{
+    this._items.push(todoitem);
+  }
+
+  _equalsDayFilter = (todoItem) => todoItem.equalsDayOfCreatedAt(this._date);
+  _notEqualsDayFilter = (todoItem) => !todoItem.equalsDayOfCreatedAt(this._date);
+  _completedFilter = (todoItem) => todoItem.completed;
+  _notCompletedFilter = (todoItem) => !todoItem.completed;
+
+  get equalsDayItems() {
+    return this._items.filter(this._equalsDayFilter);
+  }
+
+  get notEqualsDayItems() {
+    return this._items.filter(this._notEqualsDayFilter);
+  }
+
+  get equalsDayAndCompletedItems() {
+    return this.equalsDayItems.filter(this._completedFilter);
+  }
+
+  get equalsDayAndNotCompletedItems() {
+    return this.equalsDayItems.filter(this._notCompletedFilter);
+  }
+
+  get notEqualsDayAndCompletedItems() {
+    return this.notEqualsDayItems.filter(this._completedFilter);
+  }
+
+  get notEqualsDayAndNotCompletedItems() {
+    return this.notEqualsDayItems.filter(this._notCompletedFilter);
+  }
+
+
 }
+
 
 export default TodoList;
