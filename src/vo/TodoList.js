2034f909311378843db650a24a923c4e13bf3855
jhYun505 <y_jh0505@naver.com>
2022-05-09 19:50:29 +0900

    TodoItem, TodoList test code 작성

--- src/vo/TodoList.js
+++ src/vo/TodoList.js
@@ -1,4 +1,5 @@
 import { makeObservable, observable } from "mobx";
+import { ThemeConsumer } from "styled-components";
 
 class TodoList {
   /*
@@ -8,11 +9,57 @@ class TodoList {
   _items = [];
   _date = "";
 
-  constructor() {
+  constructor(items, date) {
     makeObservable(this, {
       _items: observable,
     });
+    this._items = items;
+    this._date = date;
+  }
+
+  get items() {
+    return this._items;
+  }
+  
+  removeTodoItem = (todoId) => {
+    const targetTodoItemIndex = this._items.findIndex(
+      (todo) => todo.id === todoId
+    );
+    if(targetTodoItemIndex === -1) return;
+    this._items.splice(targetTodoItemIndex, 1);
+  };
+
+  pushTodoItem = (todoItem) => {
+    this._items.push(todoItem);
+  }
+  _equalsDayFilter = (todoItem) => todoItem.equalsDayofCreadtedAt(this._date);
+
+  get equalsDayItems () {
+    return this._items.filter(this._equalsDayFilter);
+  }
+  _notEqualsDayFilter = (todoItem) => !todoItem.equalsDayofCreadtedAt(this._date);
+
+  get notEqualsDayItems () {
+    return this._items.filter(this._notEqualsDayFilter);
+  }
+
+  _completedFilter = (todoItem) => todoItem.completed;
+  _NotcompletedFilter = (todoItem) => !todoItem.completed;
+
+  get equalsDayAndCompletedItems() {
+    return this.equalsDayItems.filter(this._completedFilter);
+  }
+  get equalsDayAndNotCompletedItems() {
+    return this.equalsDayItems.filter(this._NotcompletedFilter);
+  }
+
+  get notEqualsDayAndCompletedItems() {
+    return this.notEqualsDayItems.filter(this._completedFilter);
+  }
+  get notEqualsDayAndNotCompletedItems() {
+    return this.notEqualsDayItems.filter(this._NotcompletedFilter);
   }
 }
+
 
 export default TodoList;
