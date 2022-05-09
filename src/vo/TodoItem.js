a0cc9caef03e80fff66adaba551c107189599732
juheon-jeong <jjh990427@gmail.com>
2022-05-09 21:00:06 +0900

    assignment 201802155
    assignment 201802155

--- src/vo/TodoItem.js
+++ src/vo/TodoItem.js
@@ -12,13 +12,44 @@ class TodoItem {
   _createdAt = "";
   _completed = false;
 
-  constructor() {
+  constructor(id, task, createAt, completed = false) {
     makeObservable(this, {
       _task: observable,
       _completed: observable,
     });
+    this._id = id;
+    this._task = task;
+    this._createdAt = createAt;
+    this._completed = completed;
   }
 
+  updateTask = (task) => {
+    this._task = task;
+  }
+
+  setComplete = () =>  {
+    this._completed = true;
+  }
+  unsetComplete = () => {
+    this._completed = false;
+  }
+
+  // isToday = () => {
+  //   const sourceDate = new Date(this._createdAt).setHours(0,0,0,0);
+  //   const targetDate = new Date(Date.now()).setHours(0,0,0,0);
+  //   return sourceDate === targetDate;
+  // }
+  equalsDayOfCreatedAt = (_targetDate) => {
+    const sourceDate = new Date(this._createdAt).setHours(0,0,0,0);
+    const targetDate = new Date(_targetDate).setHours(0,0,0,0);
+    return sourceDate === targetDate;
+
+  }
+  equalsCompleted = (_com) => {
+    const sourceCom = this.completed;
+    const targetCom = _com.completed;
+    return sourceCom === targetCom;
+  }
   get id() {
     return this._id;
   }
--- src/vo/TodoList.js
+++ src/vo/TodoList.js
@@ -1,4 +1,5 @@
 import { makeObservable, observable } from "mobx";
+import todoItem from "./TodoItem";
 
 class TodoList {
   /*
@@ -8,10 +9,49 @@ class TodoList {
   _items = [];
   _date = "";
 
-  constructor() {
+  constructor(array, date) {
     makeObservable(this, {
       _items: observable,
     });
+    this._items = array;
+    this._date = date;
+  }
+  removeTodoItem = (id) => {
+    const targetTodoItemIndex = this._items.findIndex(
+        (todo) => todo.id === id
+    );
+    if (targetTodoItemIndex === -1) return;
+    this._items.splice(targetTodoItemIndex, 1);
+  };
+
+  pushTodoItem = (item) => {
+    this.items.push(item);
+  }
+  _equalsDayFilter = (todoItem) => todoItem.equalsDayOfCreatedAt(this._date);
+  get equalsDayItems() {
+    return this._items.filter(this._equalsDayFilter);
+  }
+ _notequalsDayFilter = (todoItem) => !todoItem.equalsDayOfCreatedAt(this._date);
+  get notequalsDayItems() {
+    return this._items.filter(this._notequalsDayFilter)
+  }
+_completedFilter = (todoItem) => todoItem.completed;
+  get equalsDayAndCompletedItems() {
+    return this.equalsDayItems.filter(this._completedFilter);
+  }
+  _notCompletedFilter = (todoItem) => !todoItem.completed;
+  get notEqualsDayAndCompletedItems() {
+    return this.notequalsDayItems.filter(this._completedFilter);
+  }
+
+  get equalsDayAndNotCompletedItems() {
+    return this.equalsDayItems.filter(this._notCompletedFilter);
+  }
+  get notEqualsDayAndNotCompletedItems() {
+    return this.notequalsDayItems.filter(this._notCompletedFilter);
+  }
+  get items() {
+    return this._items;
   }
 }
 
