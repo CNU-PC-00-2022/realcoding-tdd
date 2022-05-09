5c9efd8135c6b58de98f1c354e0376e7ec94dcf5
kjeon0901 <kjeon0901@naver.com>
2022-05-09 17:51:06 +0900

    del - notused

--- src/__tests__/todoList.spec.js
+++ src/__tests__/todoList.spec.js
@@ -30,15 +30,6 @@ describe("할 일 목록을 가지고 있다.", () => {
 
 describe("할 일 목록에서 삭제를 할 수 있다.", () => {
   test("5개의 할 일이 있는데, id가 3인 할 일을 삭제할 수 있다", () => {
-    /*
-    const todoItem1 = new TodoItem(1, "할 일 1", new Date());
-    const todoItem2 = new TodoItem(2, "할 일 2", new Date());
-    const todoItem3 = new TodoItem(3, "할 일 3", new Date());
-    const todoItem4 = new TodoItem(4, "할 일 4", new Date());
-    const todoItem5 = new TodoItem(5, "할 일 5", new Date());
-    const todoItemList = [todoItem1, todoItem2, todoItem3, todoItem4, todoItem5];
-    const todoList = new TodoList(todoItemList);
-    */
     todoList.removeTodoItem(3);
     expect(todoList.items).toHaveLength(4);
     expect(todoList.items.some((todoItem) => todoItem.id === 3)).toBeFalsy();
@@ -47,15 +38,6 @@ describe("할 일 목록에서 삭제를 할 수 있다.", () => {
 
 describe("할 일 목록에서 추가를 할 수 있다.", () => {
   test("5개의 할 일이 있는데, id가 3인 할 일을 삭제할 수 있다", () => {
-    /*
-    const todoItem1 = new TodoItem(1, "할 일 1", new Date());
-    const todoItem2 = new TodoItem(2, "할 일 2", new Date());
-    const todoItem3 = new TodoItem(3, "할 일 3", new Date());
-    const todoItem4 = new TodoItem(4, "할 일 4", new Date());
-    const todoItem5 = new TodoItem(5, "할 일 5", new Date());
-    const todoItemList = [todoItem1, todoItem2, todoItem3, todoItem4, todoItem5];
-    const todoList = new TodoList(todoItemList);
-    */
     const todoItem6 = new TodoItem(6, "할 일 6", new Date());
 
     todoList.pushTodoItem(todoItem6);
@@ -66,30 +48,12 @@ describe("할 일 목록에서 추가를 할 수 있다.", () => {
 
 describe("생성한 할 일들 중에서 오늘 할 일, 지난 할 일 구분할 수 있다.", () => {
   test("5개의 할 일이 있는데, id 2번을 제외하고 모두 오늘 할 일이다", () => {
-    /*
-    const todoItem1 = new TodoItem(1, "할 일 1", new Date());
-    const todoItem2 = new TodoItem(2, "할 일 2", new Date('2022-05-08T10:10:10'));
-    const todoItem3 = new TodoItem(3, "할 일 3", new Date());
-    const todoItem4 = new TodoItem(4, "할 일 4", new Date());
-    const todoItem5 = new TodoItem(5, "할 일 5", new Date());
-    const todoItemList = [todoItem1, todoItem2, todoItem3, todoItem4, todoItem5];
-    const todoList = new TodoList(todoItemList, new Date());
-    */
     jest.spyOn(todoItem2, "equalsDayOfCreatedAt").mockImplementation(() => false); // 아예 모킹으로 오늘 만든 게 아니라고 알려줌
 
     expect(todoList.equalsDayItems).toHaveLength(4);
     expect(todoList.equalsDayItems.some((todoItem) => todoItem.id === 2)).toBeFalsy();
   });
   test("5개의 할 일이 있는데, id 2번만 지난 할 일이다", () => {
-    /*
-    const todoItem1 = new TodoItem(1, "할 일 1", new Date());
-    const todoItem2 = new TodoItem(2, "할 일 2", new Date('2022-05-08T10:10:10'));
-    const todoItem3 = new TodoItem(3, "할 일 3", new Date());
-    const todoItem4 = new TodoItem(4, "할 일 4", new Date());
-    const todoItem5 = new TodoItem(5, "할 일 5", new Date());
-    const todoItemList = [todoItem1, todoItem2, todoItem3, todoItem4, todoItem5];
-    const todoList = new TodoList(todoItemList, new Date());
-    */
     jest.spyOn(todoItem2, "equalsDayOfCreatedAt").mockImplementation(() => false); // 아예 모킹으로 오늘 만든 게 아니라고 알려줌
 
     expect(todoList.notEqualsDayItems).toHaveLength(1);
