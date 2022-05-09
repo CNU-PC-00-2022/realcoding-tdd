279c67d164ea6a12803aaa278a7aa6062cdb0193
Gmelon <gmelon24kr@gmail.com>
2022-05-09 17:09:19 +0900

    중간 저장

--- src/vo/TodoItem.js
+++ src/vo/TodoItem.js
@@ -1,41 +1,71 @@
 import { makeObservable, observable } from "mobx";
 
 class TodoItem {
-  /*
-    id: number (고유값)
-    task: string (할 일)
-    createdAt: Date (생성 된 날짜)
-    completed: boolean (완료 여부)
-  */
-  _id = "";
-  _task = "";
-  _createdAt = "";
-  _completed = false;
+    /*
+      id: number (고유값)
+      task: string (할 일)
+      createdAt: Date (생성 된 날짜)
+      completed: boolean (완료 여부)
+    */
+    _id = "";
+    _task = "";
+    _createdAt = "";
+    _completed = false;
 
-  constructor() {
-    makeObservable(this, {
-      _task: observable,
-      _completed: observable,
-    });
-  }
+    constructor(id, task, createdAt, completed = false) {
+        makeObservable(this, {
+            _task: observable,
+            _completed: observable,
+        });
+        this._id = id;
+        this._task = task;
+        this._createdAt = createdAt;
+        this._completed = completed;
+    }
 
-  get id() {
-    return this._id;
-  }
+    // isToday = () => {
+    //     const sourceDate = new Date(this._createdAt).setHours(0, 0, 0, 0);
+    //     const targetDate = new Date(Date.now()).setHours(0, 0, 0, 0);
 
-  get task() {
-    return this._task;
-  }
+    //     return sourceDate === targetDate;
+    // }
 
-  get completed() {
-    return this._completed;
-  }
+    equalsDayOfCreatedAt = (_targetDate) => {
+        const sourceDate = new Date(this._createdAt).setHours(0, 0, 0, 0);
+        const targetDate = new Date(_targetDate).setHours(0, 0, 0, 0);
 
-  get createdAtGMTString() {
-    const date = new Date(this._createdAt);
-    date.setHours(date.getHours() + 9);
-    return date.toISOString().replace("T", " ").substring(0, 19);
-  }
+        return sourceDate === targetDate;
+    }
+
+    updateTask = (task) => {
+        this._task = task;
+    }
+
+    setComplete = () => {
+        this._completed = true;
+    }
+
+    unsetComplete = () => {
+        this._completed = false;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get task() {
+        return this._task;
+    }
+
+    get completed() {
+        return this._completed;
+    }
+
+    get createdAtGMTString() {
+        const date = new Date(this._createdAt);
+        date.setHours(date.getHours() + 9);
+        return date.toISOString().replace("T", " ").substring(0, 19);
+    }
 }
 
 export default TodoItem;
--- src/vo/TodoList.js
+++ src/vo/TodoList.js
@@ -1,18 +1,49 @@
 import { makeObservable, observable } from "mobx";
 
 class TodoList {
-  /*
-    items: TodoItem[] (할 일 리스트)
-    date: Date (오늘 날짜)
-  */
-  _items = [];
-  _date = "";
+    /*
+      items: TodoItem[] (할 일 리스트)
+      date: Date (오늘 날짜)
+    */
+    _items = [];
+    _date = "";
 
-  constructor() {
-    makeObservable(this, {
-      _items: observable,
-    });
-  }
+    constructor(items, date) {
+        makeObservable(this, {
+            _items: observable,
+        });
+        this._items = items;
+        this._date = date;
+    }
+
+    get items() {
+        return this._items;
+    }
+
+    removeTodoItem = (todoId) => {
+        const targetTodoItemIndex = this._items.findIndex(
+            (todo) => todo.id === todoId
+        );
+        if (targetTodoItemIndex === -1) return;
+        this._items.splice(targetTodoItemIndex, 1);
+    }
+
+    pushTodoItem = (todoList) => {
+        this.items.push(todoList);
+    }
+
+    _equalsDayFilter = (todoItem) => todoItem.equalsDayOfCreatedAt(this._date);
+
+    get equalsDayItems() {
+        return this._items.filter(this._equalsDayFilter);
+    }
+
+    _notEqualsDayFilter = (todoItem) => !todoItem.equalsDayOfCreatedAt(this._date);
+
+    get notEqualsDayItems() {
+        return this._items.filter(this._notEqualsDayFilter);
+    }
+
 }
 
 export default TodoList;
