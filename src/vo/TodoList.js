769e715c19a4e5778386e9c529f2358bee5ba6f6
csk6314 <37896060+csk6314@users.noreply.github.com>
2022-05-10 21:42:58 +0900

    team-8 201802163

--- src/vo/TodoList.js
+++ src/vo/TodoList.js
@@ -8,10 +8,47 @@ class TodoList {
   _items = [];
   _date = "";
 
-  constructor() {
+  constructor(items,date) {
     makeObservable(this, {
       _items: observable,
     });
+    this._items = items
+    this._date = date;
+  }
+
+  get items() {
+    return this._items;
+  }
+
+  get equalsDayItems() {
+    return this._items.filter((todoItem)=>todoItem.equalsDayOfCreatedAt(this._date))
+  }
+
+  get notEqualsDayItems() {
+    return this._items.filter((todoItem)=>!todoItem.equalsDayOfCreatedAt(this._date))
+  }
+
+  get equalsDayAndCompletedItems() {
+    return this.equalsDayItems.filter((item)=>item.completed);
+  }
+  get equalsDayAndNotCompletedItems() {
+    return this.equalsDayItems.filter((item)=>!item.completed);
+  }
+  get notEqualsDayAndCompletedItems() {
+    return this.notEqualsDayItems.filter((item)=>item.completed);
+  }
+  get notEqualsDayAndNotCompletedItems() {
+    return this.notEqualsDayItems.filter((item)=>!item.completed);
+  }
+
+
+  removeTodoItem =(id)=>{
+    const filteredArray = this._items.filter((value)=>value.id!==id);
+    this._items = filteredArray;
+  }
+
+  pushTodoItem = (item) => {
+    this._items.push(item);
   }
 }
 
