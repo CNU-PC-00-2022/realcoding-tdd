9d2cdc08a6962c7177168bb5f14e151b9bf334d9
rjsdn0124 <201802145@o.cnu.ac.kr>
2022-05-13 14:10:01 +0900

    Revert "임동윤띠 머징"

--- src/vo/201802044/TodoList.js
+++ src/vo/201802044/TodoList.js
@@ -13,48 +13,47 @@ class TodoList {
       _items: observable,
     });
     this._items = items;
-    this._date = date
+    this._date = date;
   }
 
   _equalsDayFilter = (todoItem) => todoItem.equalsDayOfCreatedAt(this._date);
-  _notEqualsDayFilter = (todoItem) => !todoItem.equalsDayOfCreatedAt(this._date);
   _completedFilter = (todoItem) => todoItem.completed;
+  _notEqualsDayFilter = (todoItem) => !todoItem.equalsDayOfCreatedAt(this._date);
   _notCompletedFilter = (todoItem) => !todoItem.completed;
 
   get equalsDayItems() {
     return this._items.filter(this._equalsDayFilter);
+  }
+
+  get equalsDayAndCompletedItems() {
+    return this.equalsDayItems.filter(this._completedFilter);
+  }
+  
+  get equalsDayAndNotCompletedItems() {
+    return this.equalsDayItems.filter(this._notCompletedFilter);
   }
   
   get notEqualsDayItems() {
     return this._items.filter(this._notEqualsDayFilter);
   }
 
-  get equalsDayAndCompletedItems() {
+  get notEqualsDayAndCompletedItems() {
     return this.equalsDayItems.filter(this._completedFilter);
   }
 
-  get equalsDayAndNotCompletedItems() {
+  get notEqualsDayAndNotCompletedItems() {
     return this.equalsDayItems.filter(this._notCompletedFilter);
   }
 
-  get notEqualsDayAndNotCompletedItems() {
-    return this.notEqualsDayItems.filter(this._notCompletedFilter);
-  }
+  removeTodoItem = (todoId) => {
+    const targetTodoItemIndex = this._items.findIndex((todo) => todo.id === todoId);
 
-  get notEqualsDayAndCompletedItems() {
-    return this.notEqualsDayItems.filter(this._completedFilter);
-  }
-
-  removeTodoItems = (toDoId) => {
-    const targetTodoItemIndex = this._items.findIndex(
-      (todo) => todo.id === toDoId
-    );
-    if (targetTodoItemIndex === -1) return;
+    if(targetTodoItemIndex === -1) return;
     this._items.splice(targetTodoItemIndex, 1);
   }
 
-  pushTodoItem = (pushItem) => {
-    this._items.push(pushItem);
+  pushTodoItem = (todoItem) => {
+    this._items.push(todoItem);
   }
 
   get items() {
