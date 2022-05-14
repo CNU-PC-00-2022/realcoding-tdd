c5ed2e483f63fb22b8f6daf0ba14d04a108ad9ba
giwhan-kim <kgh06079@naver.com>
2022-05-14 17:12:11 +0900

    week11-과제2

--- src/vo/TodoItem.js
+++ src/vo/TodoItem.js
@@ -1,41 +1,64 @@
 import { makeObservable, observable } from "mobx";
 
 class TodoItem {
-  /*
-    id: number (고유값)
-    task: string (할 일)
-    createdAt: Date (생성 된 날짜)
-    completed: boolean (완료 여부)
-  */
-  _id = "";
-  _task = "";
-  _createdAt = "";
-  _completed = false;
+    /*
+      id: number (고유값)
+      task: string (할 일)
+      createdAt: Date (생성 된 날짜)
+      completed: boolean (완료 여부)
+    */
+    _id = "";
+    _task = "";
+    _createdAt = "";
+    _completed = false;
 
-  constructor() {
-    makeObservable(this, {
-      _task: observable,
-      _completed: observable,
-    });
-  }
+    constructor(id, task, createdAt, completed = false) {
+        makeObservable(this, {
+            _task: observable,
+            _completed: observable,
+        });
+        this._id = id;
+        this._task = task;
+        this._createdAt = createdAt;
+        this._completed = completed;
+    }
 
-  get id() {
-    return this._id;
-  }
+    updateTask = (task) => {
+        this._task = task;
+    }
 
-  get task() {
-    return this._task;
-  }
+    setComplete = () => {
+        this._completed = true;
+    }
 
-  get completed() {
-    return this._completed;
-  }
+    unsetComplete = () => {
+        this._completed = false;
+    }
 
-  get createdAtGMTString() {
-    const date = new Date(this._createdAt);
-    date.setHours(date.getHours() + 9);
-    return date.toISOString().replace("T", " ").substring(0, 19);
-  }
+    // isToday => equalsDayOfCreatedAt(_targetDate)
+    equalsDayOfCreatedAt = (_targetDate) => {
+        const sDate = new Date(this._createdAt).setHours(0, 0, 0, 0);
+        const dDate = new Date(_targetDate).setHours(0, 0, 0, 0)
+        return sDate === dDate
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get task() {
+        return this._task;
+    }
+
+    get completed() {
+        return this._completed;
+    }
+
+    get createdAtGMTString() {
+        const date = new Date(this._createdAt);
+        date.setHours(date.getHours() + 9);
+        return date.toISOString().replace("T", " ").substring(0, 19);
+    }
 }
 
 export default TodoItem;
--- src/vo/TodoList.js
+++ src/vo/TodoList.js
@@ -1,18 +1,71 @@
 import { makeObservable, observable } from "mobx";
 
 class TodoList {
-  /*
-    items: TodoItem[] (할 일 리스트)
-    date: Date (오늘 날짜)
-  */
-  _items = [];
-  _date = "";
+    /*
+      items: TodoItem[] (할 일 리스트)
+      date: Date (오늘 날짜)
+    */
+    _items = [];
+    _date = "";
 
-  constructor() {
-    makeObservable(this, {
-      _items: observable,
-    });
-  }
+    constructor(items, date) {
+        makeObservable(this, {
+            _items: observable,
+        });
+        this._items = items;
+        this._date = date;
+    }
+
+    removeTodoItem = (todoId) => {
+        const targetTodoItemIndex = this._items.findIndex(
+            (todo) => todo.id === todoId
+        );
+        if (targetTodoItemIndex === -1) return;
+        this._items.splice(targetTodoItemIndex, 1);
+    }
+
+    pushTodoItem = (todoList) => {
+        this.items.push(todoList);
+    }
+
+    get items() {
+        return this._items;
+    }
+
+    // todoList 가 생성된 날짜와 item 들의 날짜가 같은 것들을 리턴해줌([])
+    // 기존에 TodoItem.js 에서 구현해둔 equlasDayOfCreatedAt 함수를 활용
+    _equalsDayFilter = (todoItem) => todoItem.equalsDayOfCreatedAt(this._date);
+
+    get equalsDayItems() {
+        return this._items.filter(this._equalsDayFilter);
+    }
+
+    _notEqualsDayFilter = (todoItem) => !todoItem.equalsDayOfCreatedAt(this._date);
+
+    get notEqualsDayItems() {
+        return this._items.filter(this._notEqualsDayFilter);
+    }
+
+    _completedFilter = (todoItem) => todoItem.completed;
+    _notCompletedFilter = (todoItem) => !todoItem.completed;
+
+    get equalsDayAndCompletedItems() {
+        return this.equalsDayItems.filter(this._completedFilter);
+    }
+
+    get equalsDayAndNotCompletedItems() {
+        return this.equalsDayItems.filter(this._notCompletedFilter);
+    }
+
+    get notEqualsDayAndCompletedItems() {
+        return this.notEqualsDayItems.filter(this._completedFilter);
+    }
+
+    get notEqualsDayAndNotCompletedItems() {
+        return this.notEqualsDayItems.filter(this._notCompletedFilter);
+    }
+
+
 }
 
 export default TodoList;
